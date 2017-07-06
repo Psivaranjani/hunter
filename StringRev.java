4b52dd79de5aab93348dfbf72f0ac964e455378e
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-06 20:53:21 +0530

    Create StringRev.java

---
+++ StringRev.java
@@ -0,0 +1,20 @@
+# hunter
+import java.util.*;
+public class StringRev{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    String str = sc.nextLine();
+    String temp = "";
+    String finalString = "";
+    for(int i =str.length()-1;i>=0;i--){
+      temp +=i!=0?str.charAt(i):str.charAt(i)+" ";
+      if(str.charAt(i) == ' '||i==0){
+        for(int j=temp.length()-1;j>=0;j--){
+          finalString += temp.charAt(j);
+        }
+        temp = "";
+      }
+    }
+    System.out.println(finalString);
+  }
+}
