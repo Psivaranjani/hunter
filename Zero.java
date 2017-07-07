7eb23562f3aa47959c270dfdf0d75c8d84908e5d
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-07 14:24:27 +0530

    Create Zero.java

---
+++ Zero.java
@@ -0,0 +1,22 @@
+# hunter
+import java.util.*;
+public class Zero
+{
+public static void main(String ar[])throws Exception
+{
+Scanner sc=new Scanner(System.in);
+int s;
+s=sc.nextInt();
+int a[]=new int[s];
+for(int i=0;i<s;i++)
+a[i]=sc.nextInt();
+for(int i=0;i<s-1;i++)
+{
+for(int j=i+1;j<s;j++)
+{
+if(a[i]+a[j]==0)
+System.out.println("elements are"+a[i]+"\t"+a[j]);
+}
+}
+}
+}
